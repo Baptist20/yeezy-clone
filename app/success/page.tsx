ee0ab8848a91f0a3be528bb17ca5b0630a9612c0
Baptist20 <119492732+Baptist20@users.noreply.github.com>
2026-05-19 17:49:15 +0100

    corrected useSearchParams error

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
 import React from "react";
 
 export default function Success() {
-  const searchParams = useSearchParams();
-  const sessionId = searchParams.get("session_id");
+  // const searchParams = useSearchParams();
+  // const sessionId = searchParams.get("session_id");
 
   return (
     <div>
