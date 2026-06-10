795670159ee140f0ae852ffbe9549ca76c1a3d02
Baptist20 <119492732+Baptist20@users.noreply.github.com>
2026-06-10 08:41:42 +0100

    fixing images not displaying trial 2

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,29 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: { unoptimized: true },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: { unoptimized: true },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dev-yeezy-by-baptist.pantheonsite.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
